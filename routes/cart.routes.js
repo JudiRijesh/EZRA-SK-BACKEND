3b26c0e523c30a88d360d777f23b33f2380e99ac
Mary Judi Kavitha Karunakaran <kavitha.rijesh@gmail.com>
2024-11-06 20:39:08 +0100

    route updated for cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -2,30 +2,45 @@ const { isAuthenticated } = require('../middleware/jwt.middleware');
 const Cart = require('../models/Cart.model');
 const router = require('express').Router();
 
-router.post('/cart',isAuthenticated, async (req, res) => {
+router.post('/cart', isAuthenticated, async (req, res) => {
   try {
-    const {name,description} = req.body
-    const {_id} = req.payload
-    const createdCart = await Cart.create({userId:_id,services:{name,description}});
-    res.status(201).json(createdCart); 
-  } catch (err) {
-    console.error("Error creating contact:", err);
-    res.status(500).json({ error: "Failed to create contact", details: err }); 
-  }
-});
+    const { name, description } = req.body
+    const { _id } = req.payload
 
-router.get('/cart',isAuthenticated,async(req,res)=>{
-  console.log(req.payload)
-  const {_id} = req.payload
+   //Finding if the user has a cart
+    let cart = await Cart.findOne({ userId: _id })
+
+    // If the user doesn't have cart, create one
+    if (!cart) {
+      cart = await Cart.create({
+        userId: _id,
+        services: [{ name, description }],
+      })
+      return res.status(201).json(cart)
+    }
+
+    // If cart exists, push new service to it
+    cart.services.push({ name, description })
+    await cart.save()
+
+    res.status(200).json(cart) 
+  } catch (err) {
+    console.error("Error adding service to cart:", err)
+    res.status(500).json({ error: "Failed to add service to cart", details: err })
+  }
+})
+
+router.get('/cart', isAuthenticated, async (req, res) => {
+  const { _id } = req.payload
 
   try {
-    const allCart = await Cart.findOne({userId:_id}); 
-    console.log(allCart)
-    res.json(allCart); 
+    const allCart = await Cart.findOne({ userId: _id })
+
+    res.json(allCart)
   } catch (err) {
-    res.status(500).json(err);
-    console.log(err)
+    console.error("Error fetching cart:", err)
+    res.status(500).json({ error: "Failed to fetch cart", details: err })
   }
-});
+})
 
 module.exports = router;
